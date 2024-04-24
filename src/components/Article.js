120e7ce69db0bfbaf9ccc3ced1507702edde3f80
joel langat <langatjoel42@gmail.com>
2024-04-24 17:05:44 +0300

    made some solutions

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+// Article.js
+import React from 'react';
+
+const Article = ({ title, date, preview }) => {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date || "January 1, 1970"}</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
